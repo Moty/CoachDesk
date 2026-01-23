d945f9c0247af8ef6cbcacf133c50349301c2e1b
Mordechai (Moty) Moshin <moty.moshin@gmail.com>
2026-01-22 17:56:06 -0800

    feat: US-029 - Event-Driven Notification Handlers

---
+++ src/shared/events/index.ts
@@ -0,0 +1,8 @@
+export { EventBus, eventBus } from './EventBus.js';
+export { NotificationHandlers } from './NotificationHandlers.js';
+export type {
+  TicketCreatedEvent,
+  CommentAddedEvent,
+  TicketUpdatedEvent,
+  EventTypes,
+} from './EventBus.js';
